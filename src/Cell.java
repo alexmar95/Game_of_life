3a494349cab5d56d255d7b09ffc325e861551ea1
Tudor Stefan <tstefan@bitdefender.com>
2017-10-26 15:37:11 +0300

    Added T_full and T_starve variables.

--- src/Cell.java
+++ src/Cell.java
@@ -1,6 +1,9 @@
 
 public class Cell implements Runnable {
 
+	final static int T_full = 5000;
+	final static int T_starve = 2000;
+	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
