b0b2f9a5a41f910846f23afc70dd5cf417755cef
Tudor Stefan <tstefan@bitdefender.com>
2017-10-26 15:39:56 +0300

    Added T_starve and T_full

--- src/Cell.java
+++ src/Cell.java
@@ -2,7 +2,7 @@
 public class Cell implements Runnable {
 
 	final static int T_full = 5000;
-	final static int T_starve = 2000;
+	final static int T_starve = 3000;
 	
 	@Override
 	public void run() {
